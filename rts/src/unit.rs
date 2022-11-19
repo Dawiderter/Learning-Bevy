19afa6c6a42997e8e6192adad5a904b632c8771e
Dawiderter <dwalczak456@gmail.com>
2022-11-19 13:04:37 +0100

    RZEPA RTS RZEPA RTS BOTTOM TEXT

--- rts/src/unit.rs
+++ rts/src/unit.rs
@@ -1,26 +1,34 @@
+use bevy::gltf::Gltf;
 use bevy::prelude::*;
+use bevy::scene::SceneBundle;
 use bevy_kira_audio::prelude::*;
 use bevy_mod_picking::*;
 use leafwing_input_manager::prelude::*;
 
 pub struct UnitPlugin;
 
+#[derive(Component)]
+struct Rzepa;
+
 fn setup_unit(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut ambient_light: ResMut<AmbientLight>,
+    server: Res<AssetServer>,
 ) {
+    let rzepa: Handle<Scene> = server.load("models/rzepa.gltf#Scene0");
+
     commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        SceneBundle {
+            scene: rzepa,
+            transform: Transform::from_xyz(0.0, 0.5, 0.0).with_rotation(Quat::from_rotation_x(-45.)),
             ..default()
         },
         PickableBundle::default(),
+        meshes.add(Mesh::from(shape::Cube {size: 1.0})),
+        Rzepa,
     ));
-    // camera
 }
 
 impl Plugin for UnitPlugin {
