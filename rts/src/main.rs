0db8567945fd40c40112c51658b14a9d7e363f95
Dawiderter <dwalczak456@gmail.com>
2022-11-19 07:37:46 +0100

    Removed artifacts

--- rts/src/main.rs
+++ rts/src/main.rs
@@ -8,37 +8,6 @@ use ui::UiPlugin;
 mod camera;
 mod ui;
 
-/// set up a simple 3D scene
-fn setup_cubes(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut ambient_light: ResMut<AmbientLight>,
-) {
-    ambient_light.color = Color::WHITE;
-    ambient_light.brightness = 1.0;
-    // plane
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 100.0 })),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-            ..default()
-        },
-        PickableBundle::default(),
-    ));
-    // cube
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        },
-        PickableBundle::default(),
-    ));
-    // camera
-}
-
 use board::BoardPlugin;
 mod board;
 
